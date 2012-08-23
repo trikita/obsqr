f49ad5642689a178a382c43926aa9d5480d30f5a
Anna Kruglik <devnull@localhost>
2012-08-23 20:17:52 +0300

    Fixed url parser

--- src/trikita/obsqr/QrParser.java
+++ src/trikita/obsqr/QrParser.java
@@ -411,6 +411,7 @@ public class QrParser {
 	/* ----------------------- QR type: url --------------------- */
 	private static class QrContentUrl extends BaseQrContent {
 		private Context mContext;
+
 		private final static String URL_REGEX = "((https?|ftp)\\:\\/\\/)?" + // SCHEME
 			"([a-z0-9+!*(),;?&=\\$_.-]+(\\:[a-z0-9+!*(),;?&=\\$_.-]+)?@)?" + // User and Pass
 			"([a-z0-9-.]*)\\.([a-z]{2,3})" + // Host or IP
@@ -418,6 +419,67 @@ public class QrParser {
 			"(\\/([a-z0-9+\\$_-]\\.?)+)*\\/?" + // Path
 			"(\\?[a-z+&\\$_.-][a-z0-9;:@&%=+\\/\\$_.-]*)?" + // GET Query
 			"(#[a-z_.-][a-z0-9+\\$_.-]*)?"; // Anchor
+
+		public static final String TOP_LEVEL_DOMAIN_STR_FOR_WEB_URL =
+			"(?:"
+			+ "(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])"
+			+ "|(?:biz|b[abdefghijmnorstvwyz])"
+			+ "|(?:cat|com|coop|c[acdfghiklmnoruvxyz])"
+			+ "|d[ejkmoz]"
+			+ "|(?:edu|e[cegrstu])"
+			+ "|f[ijkmor]"
+			+ "|(?:gov|g[abdefghilmnpqrstuwy])"
+			+ "|h[kmnrtu]"
+			+ "|(?:info|int|i[delmnoqrst])"
+			+ "|(?:jobs|j[emop])"
+			+ "|k[eghimnprwyz]"
+			+ "|l[abcikrstuvy]"
+			+ "|(?:mil|mobi|museum|m[acdeghklmnopqrstuvwxyz])"
+			+ "|(?:name|net|n[acefgilopruz])"
+			+ "|(?:org|om)"
+			+ "|(?:pro|p[aefghklmnrstwy])"
+			+ "|qa"
+			+ "|r[eosuw]"
+			+ "|s[abcdeghijklmnortuvyz]"
+			+ "|(?:tel|travel|t[cdfghjklmnoprtvwz])"
+			+ "|u[agksyz]"
+			+ "|v[aceginu]"
+			+ "|w[fs]"
+			+ "|(?:xn\\-\\-0zwm56d|xn\\-\\-11b5bs3a9aj6g|xn\\-\\-80akhbyknj4f|xn\\-\\-9t4b11yi5a|xn\\-\\-deba0ad|xn\\-\\-g6w251d|xn\\-\\-hgbk6aj7f53bba|xn\\-\\-hlcj6aya9esc7a|xn\\-\\-jxalpdlp|xn\\-\\-kgbechtv|xn\\-\\-zckzah)"
+			+ "|y[etu]"
+			+ "|z[amw]))";
+
+		/**
+		 * Good characters for Internationalized Resource Identifiers (IRI).
+		 * This comprises most common used Unicode characters allowed in IRI
+		 * as detailed in RFC 3987.
+		 * Specifically, those two byte Unicode characters are not included.
+		 */
+		public static final String GOOD_IRI_CHAR =
+			"a-zA-Z0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF";
+
+		/**
+		 *  Regular expression pattern to match most part of RFC 3987
+		 *  Internationalized URLs, aka IRIs.  Commonly used Unicode characters are
+		 *  added.
+		 */
+		public static final Pattern WEB_URL = Pattern.compile(
+			"((?:(http|https|Http|Https|rtsp|Rtsp):\\/\\/(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\'\\(\\)"
+			+ "\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,64}(?:\\:(?:[a-zA-Z0-9\\$\\-\\_"
+			+ "\\.\\+\\!\\*\\'\\(\\)\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,25})?\\@)?)?"
+			+ "((?:(?:[" + GOOD_IRI_CHAR + "][" + GOOD_IRI_CHAR + "\\-]{0,64}\\.)+"   // named host
+			+ TOP_LEVEL_DOMAIN_STR_FOR_WEB_URL
+			+ "|(?:(?:25[0-5]|2[0-4]" // or ip address
+			+ "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(?:25[0-5]|2[0-4][0-9]"
+			+ "|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(?:25[0-5]|2[0-4][0-9]|[0-1]"
+			+ "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+			+ "|[1-9][0-9]|[0-9])))"
+			+ "(?:\\:\\d{1,5})?)" // plus option port number
+			+ "(\\/(?:(?:[" + GOOD_IRI_CHAR + "\\;\\/\\?\\:\\@\\&\\=\\#\\~"  // plus option query params
+			+ "\\-\\.\\+\\!\\*\\'\\(\\)\\,\\_])|(?:\\%[a-fA-F0-9]{2}))*)?"
+			+ "(?:\\b|$)"); // and finally, a word boundary or end of
+							// input.  This is to stop foo.sure from
+							// matching as foo.su
 
 		public QrContentUrl(Context ctx, String s) {
 			mContext = ctx;
@@ -443,8 +505,9 @@ public class QrParser {
 		}
 
 		public static boolean match(String s) {
-			Pattern pattern = Pattern.compile(URL_REGEX);
-			Matcher matcher = pattern.matcher(s);
+			//Pattern pattern = Pattern.compile(URL_REGEX);
+			//Matcher matcher = pattern.matcher(s);
+			Matcher matcher = WEB_URL.matcher(s);
 			return matcher.matches();
 		}
 	}
