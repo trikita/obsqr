22391afe6b2787e6bfdaa544dcac326946dcfaae
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2014-07-12 17:02:36 +0300

    fixed NDK warnings, automated release builds

--- src/main/jni/iconv.c
+++ src/main/jni/iconv.c
@@ -7,7 +7,7 @@ iconv_t iconv_open (const char *tocode, const char *fromcode) {
 	return NULL;
 }
 
-size_t iconv (iconv_t cd, const char **inbuf, size_t *inbytesleft, 
+size_t iconv (iconv_t cd, char **inbuf, size_t *inbytesleft, 
 		char **outbuf, size_t *outbytesleft) {
 	size_t sz = *inbytesleft;
 	memcpy(*outbuf, *inbuf, sz);
--- src/main/jni/iconv.h
+++ src/main/jni/iconv.h
@@ -3,7 +3,7 @@
 
 typedef void *iconv_t;
 extern iconv_t iconv_open (const char *tocode, const char *fromcode);
-extern size_t iconv (iconv_t cd, const char **inbuf,
+extern size_t iconv (iconv_t cd, char **inbuf,
                      size_t *inbytesleft, char **outbuf, size_t *outbytesleft);
 extern int iconv_close (iconv_t cd);
 
