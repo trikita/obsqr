afd76d35e2c04405ea1b1b581aaf445194824028
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2014-07-18 23:45:39 +0300

    added scrollable view for long text

---
+++ src/main/java/trikita/obsqr/WrappingScrollView.java
@@ -0,0 +1,29 @@
+package trikita.obsqr;
+
+import android.content.Context;
+import android.widget.ScrollView;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class WrappingScrollView extends ScrollView {
+
+	public final static int MAX_HEIGHT = 120;
+
+	public WrappingScrollView(Context c) {
+		this(c, null);
+	}
+
+	public WrappingScrollView(Context c, AttributeSet attrs) {
+		this(c, attrs, 0);
+	}
+
+	public WrappingScrollView(Context c, AttributeSet attrs, int defStyle) {
+		super(c, attrs, defStyle);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(MAX_HEIGHT, MeasureSpec.AT_MOST);
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+}
